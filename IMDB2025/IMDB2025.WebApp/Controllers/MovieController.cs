f6eb753b85866b9c3eb0039f9330f23a41e1d336
Yuriy Muzychuk <yuriy.muzychuk@gmail.com>
2025-12-09 11:37:25 +0200

    mvc 1

---
+++ IMDB2025/IMDB2025.WebApp/Controllers/MovieController.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using IMDB2025.BL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMDB2025.WebApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieManager _manager;
+        private readonly IMapper _mapper;
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(IMovieManager manager, IMapper mapper, ILogger<MovieController> logger)
+        {
+            _manager = manager;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        // GET: MovieController
+        public ActionResult Index()
+        {
+            var movies = _manager.GetAllMovies();
+            return View(movies);
+        }
+
+        // GET: MovieController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MovieController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MovieController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MovieController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MovieController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MovieController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MovieController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ IMDB2025/IMDB2025.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IMDB2025.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
